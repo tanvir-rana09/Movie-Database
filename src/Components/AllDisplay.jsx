81aaa1e99e88c0c3b304a293073921e68ae0efef
tanvirrana <tanvir.rana01922@gmail.com>
2024-02-02 20:42:58 +0600

    fix search issue

--- src/Components/AllDisplay.jsx
+++ src/Components/AllDisplay.jsx
@@ -5,8 +5,9 @@ import { fetchDataFromApi } from '../Utils/ApiCall'
 import { useDispatch, useSelector } from 'react-redux'
 import { setLoading, setAllData } from '../Redux/AllData'
 import { SlArrowRight } from "react-icons/sl";
+import { TbMoodEmpty } from 'react-icons/tb'
 
-const AllDisplay = ({ category, title, params, paths = '', classname, media,overview=false }) => {
+const AllDisplay = ({ category, title, params, paths = '', classname, media, overview = false }) => {
 	const dispatch = useDispatch()
 	const [pagination, setPagination] = useState(1)
 	const allData = useSelector((state) => state.allData.allData)
@@ -17,14 +18,11 @@ const AllDisplay = ({ category, title, params, paths = '', classname, media,over
 		dispatch(setLoading(true))
 		try {
 			fetchDataFromApi(`${category}?page=${pagination}`).then((res) => {
-
 				dispatch(setAllData(res.results))
-				dispatch(setLoading(false))
-			})
+			}).finally(dispatch(setLoading(false)))
 		} catch (error) {
 			console.log(error);
 		}
-		// cheackMedia()
 	}, [category, dispatch, pagination])
 
 	const inc = () => {
@@ -37,12 +35,9 @@ const AllDisplay = ({ category, title, params, paths = '', classname, media,over
 		} else setPagination(1)
 	}
 
-
 	const cheackMedia = (item) => {
 		return item.first_air_date ? 'tv' : 'movie'
 	}
-
-	console.log(allData);
 
 	return (
 		<div>
@@ -54,23 +49,30 @@ const AllDisplay = ({ category, title, params, paths = '', classname, media,over
 			{
 				loader ? <Loading /> : (
 					<div>
-						<div></div>
-						<div className={`${classname ? classname : 'grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-5 '} mt-10 px-2 md:px-5 ${overview?'2xl:px-[30%] xl:px-[15%]':'2xl:px-[15%]'}`}>
+						<div className={`${classname ? classname : 'grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-5 '} mt-10 px-2 md:px-5 ${overview ? '2xl:px-[30%] xl:px-[15%]' : '2xl:px-[15%]'}`}>
 							{
-								allData?.filter((item) => item.media_type !== 'person').map((item) => (
-									<div key={item.id} className='w-full h-full'>
-										<Card item={item} media={media || cheackMedia(item)} overview={overview} />
-									</div>
-								))
+								allData.length > 0 ? (
+									allData?.filter((item) => item.media_type !== 'person').map((item) => (
+										<div key={item.id} className='w-full h-full'>
+											<Card item={item} media={media || cheackMedia(item)} overview={overview} />
+										</div>
+									))
+								) : <div >
+									<p className="linear2 text-4xl flex justify-center items-center h-[40vh] capitalize">Sorry We Can&apos;t found</p>
+									<p className="text-5xl flex items-center justify-center py-20 -mt-56 mb-56"><TbMoodEmpty color="yellow" /></p>
+								</div>
 							}
 						</div>
-						<div className='grid place-content-center my-5 h-full'>
-							<div className='flex items-center border'>
-								<button className=' px-4 py-2 hover:bg-cyan-500 bg-white text-black font-semibold hover:text-white duration-300 tracking-wider ' onClick={dec}>Prev Page</button>
-								<div className='px-6 py-2 bg-white text-black'>{pagination}</div>
-								<button className=' px-4 py-2 hover:bg-cyan-500 bg-white text-black font-semibold hover:text-white grid duration-300 tracking-wider ' onClick={inc}>Next Page</button>
+						{allData.length > 20 ? (
+							<div className='grid place-content-center my-5 h-full'>
+								<div className='flex items-center border'>
+									<button className=' px-4 py-2 hover:bg-cyan-500 bg-white text-black font-semibold hover:text-white duration-300 tracking-wider ' onClick={dec}>Prev Page</button>
+									<div className='px-6 py-2 bg-white text-black'>{pagination}</div>
+									<button className=' px-4 py-2 hover:bg-cyan-500 bg-white text-black font-semibold hover:text-white grid duration-300 tracking-wider ' onClick={inc}>Next Page</button>
+								</div>
 							</div>
-						</div>
+						) : null
+						}
 					</div>
 				)
 			}
